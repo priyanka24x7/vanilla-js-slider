722ed7ea458eb1562d4dda2505e918f9eb3390b2
Suhel Khan <suhel@ketto.org>
2021-02-16 23:42:00 +0530

    active css added

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 
 (function () {
   // Some amazing code...
-  document.write('hello world');
+  const mainWrapper = document.getElementsByClassName('carousel__photo');
+  console.log(mainWrapper);
+
+  // Next and Prev buttons
+  const next = document.getElementsByClassName('carousel__button--next')[0];
+  const prev = document.getElementsByClassName('carousel__button--prev')[0];
+
+  next.addEventListener('click', function() {
+    mainWrapper[0].classList.remove('active');
+    mainWrapper[1].classList.add('active');
+  })
 })();
