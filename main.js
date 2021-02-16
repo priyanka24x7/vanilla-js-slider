d80b0061194ff6f72c743b80cb9945918da7256a
pinkyweb <priyawebweb@gmail.com>
2021-02-16 23:57:29 +0530

    button features added
    next prev action added

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
 
 (function () {
   // Some amazing code...
+  activeIndex = 0;
   const mainWrapper = document.getElementsByClassName('carousel__photo');
-  console.log(mainWrapper);
+  console.log(mainWrapper.length);
 
   // Next and Prev buttons
   const next = document.getElementsByClassName('carousel__button--next')[0];
   const prev = document.getElementsByClassName('carousel__button--prev')[0];
 
   next.addEventListener('click', function() {
-    mainWrapper[0].classList.remove('active');
-    mainWrapper[1].classList.add('active');
+    mainWrapper[activeIndex].classList.remove('active');
+    activeIndex++;
+      if(mainWrapper.length > activeIndex ){
+        mainWrapper[activeIndex].classList.add('active');
+      }else{
+        activeIndex = 0;
+        mainWrapper[activeIndex].classList.add('active'); 
+      }
+  })
+  
+  prev.addEventListener('click', function() {
+    mainWrapper[activeIndex].classList.remove('active');
+    activeIndex++;
+      if(mainWrapper.length > activeIndex ){
+        mainWrapper[activeIndex].classList.add('active');
+      }else{
+        activeIndex = 0;
+        mainWrapper[activeIndex].classList.add('active'); 
+      }
   })
 })();
